fe47bf13e8e59007d2e6046eb4852124aefb2aeb
Anson Li <hello@ansonli.ca>
2017-06-14 15:35:27 -0600

    Rearranged order of buttons on index page

--- qmlti/src/lti/index.php
+++ qmlti/src/lti/index.php
@@ -233,8 +233,8 @@ EOD;
         </div>
         <br><br>
         <p>
-          <input id="id_delete" type="submit" class="btn btn-default" name="action" value="Delete" onclick="return confirmDelete();" />&nbsp;&nbsp;&nbsp;
-          <input id="id_configure" type="submit" class="btn btn-default" name="action" value="Configure" />
+          <input id="id_configure" type="submit" class="btn btn-default" name="action" value="Configure" />&nbsp;&nbsp;&nbsp;
+          <input id="id_delete" type="submit" class="btn btn-default" name="action" value="Delete" onclick="return confirmDelete();" />
         </p>
 
         </form>
